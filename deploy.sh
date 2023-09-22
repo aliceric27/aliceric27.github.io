50a9c0e4a5099a54eb0bd4b824e6d1ddaa1a8c25
aliceric27 <aliceric27@gmail.com>
2023-09-22 21:07:34 +0800

    Update blog - 2023-09-22 21:07:34

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 hexo clean && hexo g && hexo deploy
 git add .
-git commit -m "Update blog"
+current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "Update blog - $current_date_time"
 git push origin main
