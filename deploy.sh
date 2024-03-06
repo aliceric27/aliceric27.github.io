70dd3c9da529580bd84ec32b7e93879f63b79f6f
aliceric27 <aliceric27@gmail.com>
2024-03-06 18:35:59 +0800

    Update blog - 2024-03-06 18:35:59

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
-sed -i "s/message: \"[^\"]*\"/message: \"Update blog - $current_date_time\"/" _config.yml
-hexo clean && hexo g && hexo deploy
-git add .
+#sed -i "s/message: \"[^\"]*\"/message: \"Update blog - $current_date_time\"/" _config.yml
+git status
 git commit -m "Update blog - $current_date_time"
 git push origin main
+hexo clean && hexo g && hexo deploy
